901249c3cb2a1e650e2720ccd6bcf7440da557e2
304 <a30404@iMac-2.local>
2020-07-13 14:41:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.firstapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int a = 10;
+        int [] ar = {100,200,300};
+
+        int ab = a + ar[0];
+    }
+}
