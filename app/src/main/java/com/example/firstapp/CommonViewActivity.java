6f7170231ac5961d4aaaad3de53bad9306adf74a
binkug <binkug@naver.com>
2020-07-14 17:44:57 +0900

    View 를 배웠음

---
+++ app/src/main/java/com/example/firstapp/CommonViewActivity.java
@@ -0,0 +1,23 @@
+package com.example.firstapp;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CommonViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_view);
+
+        //TextView 가져오기
+        TextView txtmsg = (TextView)findViewById(R.id.txtmsg);
+        //assets에 있는 폰트 파일을 이용해서 폰트를 생성
+        Typeface tf = Typeface.createFromAsset(getAssets(),"nanumgothic.ttf");
+        txtmsg.setTypeface(tf);
+
+    }
+}
---
+++ app/src/main/java/com/example/firstapp/LinearActivity.java
@@ -0,0 +1,14 @@
+package com.example.firstapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
